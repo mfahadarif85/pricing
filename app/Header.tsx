98a9a719e378ab44db69b51cf6658972a2a59d62
mfahadarif85 <mfahadarif@gmail.com>
2023-02-20 17:18:38 +0500

    Pricing Section

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,21 +4,22 @@ import { Box, Heading, Text } from "@chakra-ui/react";
 
 const Header = () => {
   return (
-    <Box
-      as='section'
-      textAlign='center'
-      color='#F7FAFC'
-      bg='#6B46C1'
-      pt='90px'
-      pb='190px'
-      px='32px'
-    >
-      <Heading fontWeight='800' fontSize='48px'>
-        Simple pricing for your business
-      </Heading>
-      <Text fontWeight='500' fontSize='24px' pt='16px'>
-        Plans that are carefully crafted to suit your business.
-      </Text>
+    <Box as='section' pb='112px'>
+      <Box
+        textAlign='center'
+        color='#F7FAFC'
+        bg='#6B46C1'
+        pt='90px'
+        pb='190px'
+        px='32px'
+      >
+        <Heading fontWeight='800' fontSize='48px'>
+          Simple pricing for your business
+        </Heading>
+        <Text fontWeight='500' fontSize='24px' pt='16px'>
+          Plans that are carefully crafted to suit your business.
+        </Text>
+      </Box>
     </Box>
   );
 };
--- app/Pricing.tsx
+++ app/Pricing.tsx
@@ -1,7 +1,68 @@
+"use client";
 import React from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Text,
+  HStack,
+  Icon,
+  StackProps,
+  Stack,
+} from "@chakra-ui/react";
+import { CheckIcon } from "@/public/Icon";
+
+export const ListItem = (props: StackProps) => {
+  const { children, ...rest } = props;
+  return (
+    <HStack as='li' spacing='20px' {...rest}>
+      <Icon as={CheckIcon} w='22px' h='22px' />
+      <Text>{children}</Text>
+    </HStack>
+  );
+};
 
 const Pricing = () => {
-  return <div>Pricing</div>;
+  return (
+    <Box
+      maxWidth='994px'
+      margin='auto'
+      mt='-256px'
+      borderRadius='12px'
+      overflow='hidden'
+      boxShadow='xl'
+    >
+      <Flex textAlign='center'>
+        <Box bg='#F0EAFB' p='60px'>
+          <Text fontSize='24px' fontWeight='800'>
+            Premium PRO
+          </Text>
+          <Heading as='h3' fontSize='60px' mt='16px'>
+            $329
+          </Heading>
+          <Text color='#171923' fontSize='18px' mt='8px'>
+            billed just once
+          </Text>
+          <Button colorScheme='purple' size='lg' w='282px' mt='24px'>
+            Get Started
+          </Button>
+        </Box>
+        <Box p='60px' fontSize='18px' bg='white'>
+          <Text textAlign='left'>
+            Access these features when you get this pricing package for your
+            business.
+          </Text>
+          <Stack as='ul' spacing='16px' pt='24px'>
+            <ListItem>International calling and messaging API</ListItem>
+            <ListItem>Additional phone numbers</ListItem>
+            <ListItem>Automated messages via Zapier</ListItem>
+            <ListItem>24/7 support and consulting</ListItem>
+          </Stack>
+        </Box>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Pricing;
